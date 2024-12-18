6e83f6855a17f7457938862c2ae3e031c9739138
Kupán Zalán <zalan_kupan@yahoo.com>
2024-12-18 21:33:22 +0200

    Finalized frontend and backend connection

--- src/components/validator-table/validator-table.tsx
+++ src/components/validator-table/validator-table.tsx
@@ -12,9 +12,10 @@ export function ValidatorTable({ validators }: { validators: Validator[] }) {
         <thead className="text-s uppercase bg-gray-50 dark:bg-gray-100 dark:text-gray-100">
           <tr className="bg-[rgb(73,181,193)]">
             <th scope="col" className="px-6 py-3 text-left">ID & Name</th>
-            <th scope="col" className="px-6 py-3 text-left">Uptime</th>
-            <th scope="col" className="px-6 py-3 text-left">Fees</th>
-            <th scope="col" className="px-6 py-3 text-left">Location</th>
+            <th scope="col" className="px-6 py-3 text-left">Activated stake</th>
+            <th scope="col" className="px-6 py-3 text-left">Root distance</th>
+            <th scope="col" className="px-6 py-3 text-left">Vote distance</th>
+            <th scope="col" className="px-6 py-3 text-left">Commission</th>
           </tr>
         </thead>
         {/* Table Body */}
@@ -22,15 +23,14 @@ export function ValidatorTable({ validators }: { validators: Validator[] }) {
           {validators.map((validator) => (
             <tr key={validator.id} className="bg-[rgba(44,34,62,255)] border-b dark:border-gray-700 hover:bg-[rgba(52,40,70,255)]">
               <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                    {/* <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image"></img> */}
                     <div className="ps-3">
-                        <div className="text-base font-semibold">{validator.id}</div>
-                        <div className="font-normal text-gray-500">{validator.name}</div>
+                        <div className="text-base font-semibold">{validator.vote_account_info.nodePubkey}</div>
                     </div>  
               </th>
-              <td className="px-6 py-4">{validator.uptime}</td>
-              <td className="px-6 py-4">{validator.fees}</td>
-              <td className="px-6 py-4">{validator.location}</td>
+              <td className="px-6 py-4">{validator.vote_account_info.activatedStake}</td>
+              <td className="px-6 py-4">{validator.root_distance}</td>
+              <td className="px-6 py-4">{validator.vote_distance}</td>
+              <td className="px-6 py-4">{validator.vote_account_info.commission}</td>
             </tr>
           ))}
         </tbody>
